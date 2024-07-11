93d52f1c5d1f345f1be86dec80def573f1448cd6
Mayuri <mayurikokane318@gmail.com>
2024-07-11 18:03:50 +0530

    add user module changes

--- resources/views/master_data/user/_update.blade.php
+++ resources/views/master_data/user/_update.blade.php
@@ -1,43 +1,45 @@
 <x-base-layout>
-<style>
-    .error
-    {
-        color:#f00!important;
-    }
-</style>
-<div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
-    @if (Session::has('success'))
-        <div class="alert alert-success">{{ Session::get('success') }}</div>        
-    @endif
-    @if (Session::has('danger'))
-    <div class="alert alert-danger">{{ Session::get('danger') }}</div>
-    @endif
-    <!--begin::Card header-->
-    <div class="card-header cursor-pointer">
-        <!--begin::Card title-->
-        <div class="card-title m-0">
-            <h3 class="fw-bolder m-0">{{ __('Update User') }} - {{ $userData->first_name }}</h3>
+    <style>
+        .error {
+            color: #f00 !important;
+        }
+    </style>
+    <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+        @if (Session::has('success'))
+            <div class="alert alert-success">{{ Session::get('success') }}</div>
+        @endif
+        @if (Session::has('danger'))
+            <div class="alert alert-danger">{{ Session::get('danger') }}</div>
+        @endif
+        <!--begin::Card header-->
+        <div class="card-header cursor-pointer">
+            <!--begin::Card title-->
+            <div class="card-title m-0">
+                <h3 class="fw-bolder m-0">{{ __('Update User') }} - {{ $userData->first_name }}</h3>
+            </div>
+            <!--end::Card title-->
         </div>
-        <!--end::Card title-->
-    </div>
 
-    <!--begin::Card-->
-    <div class="card">
-        <!--begin::Card body-->
-        <div class="card-body pt-6">
-            <div class="row">
-                <!-- <div class="col-md-2"></div> -->
-                        <div class="col-md-12">
-                            <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
-                                <!--begin::Card body-->
-                                <div class="card-body p-9">
-                                <form name="create_employee" action="{{ url('update_user', $userData->id) }}" method="POST"> 
-                                    @csrf   
+        <!--begin::Card-->
+        <div class="card">
+            <!--begin::Card body-->
+            <div class="card-body pt-6">
+                <div class="row">
+                    <!-- <div class="col-md-2"></div> -->
+                    <div class="col-md-12">
+                        <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+                            <!--begin::Card body-->
+                            <div class="card-body p-9">
+                                <form name="create_employee" action="{{ url('update_user', $userData->id) }}"
+                                    method="POST">
+                                    @csrf
                                     <div class="row gy-4">
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label class="py-2" for="first_name">First Name*</label>
-                                                <input type="text" class="form-control" id="first_name" aria-describedby="name" value="{{ $userData->first_name }}" name="first_name" placeholder="First Name">
+                                                <input type="text" class="form-control" id="first_name"
+                                                    aria-describedby="name" value="{{ $userData->first_name }}"
+                                                    name="first_name" placeholder="First Name">
                                                 @error('first_name')
                                                     <span class="text-danger">please enter first name</span>
                                                 @enderror
@@ -46,7 +48,9 @@
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label class="py-2" for="last_name">Last Name*</label>
-                                                <input type="text" class="form-control" id="last_name" aria-describedby="name" name="last_name" value="{{ $userData->last_name }}" placeholder="Last Name">
+                                                <input type="text" class="form-control" id="last_name"
+                                                    aria-describedby="name" name="last_name"
+                                                    value="{{ $userData->last_name }}" placeholder="Last Name">
                                                 @error('last_name')
                                                     <span class="error">Please enter last name</span>
                                                 @enderror
@@ -55,13 +59,16 @@
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label class="py-2" for="email">Email*</label>
-                                                <input type="text" class="form-control" id="email" name="email" aria-describedby="email" placeholder="Enter email" value="{{ $userData->email }}" autocomplete="off">
+                                                <input type="text" class="form-control" id="email" name="email"
+                                                    aria-describedby="email" placeholder="Enter email"
+                                                    value="{{ $userData->email }}" autocomplete="off">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label class="py-2" for="password">Password</label>
-                                                <input type="password" class="form-control" id="password" aria-describedby="password" name="password" autocomplete="off">
+                                                <input type="password" class="form-control" id="password"
+                                                    aria-describedby="password" name="password" autocomplete="off">
                                                 @error('password')
                                                     <span class="error">Please enter user password</span>
                                                 @enderror
@@ -69,34 +76,41 @@
                                         </div>
                                         <div class="col-md-6">
                                             <label class="py-2" for="role_id">User Role*</label>
-                                            <select class="form-select" id="role_id" aria-label="Default select example" name="role_id">
+                                            <select class="form-select" id="role_id" aria-label="Default select example"
+                                                name="role_id">
                                                 <!-- <option selected>-None-</option> -->
                                                 @foreach($roles as $role)
-                                                <option value="{{ $role->id }}" {{ ($role->id==$userData->role_id)?'selected':'' }}>{{ $role->name }}</option>
+                                                    <option value="{{ $role->id }}" {{ ($role->id == $userData->role_id) ? 'selected' : '' }}>{{ $role->name }}
+                                                    </option>
                                                 @endforeach
                                             </select>
                                             @error('role_id')
                                                 <span class="error">Please select user role</span>
                                             @enderror
                                         </div>
-                                      
-                                        <div class="col-md-12">
-                                            <div class="float-end mt-3">
-                                                <!-- <button type="button" class="btn btn-sm btn-light me-3">Reset</button> -->
-                                                <button type="submit"  class="btn btn-sm btn-primary me-3">Submit</button>
-                                            </div>
+
+                                        <div class="d-flex justify-content-end align-items-center mt-12">
+                                            <!--begin::Button-->
+                                            <a href="{{ route('users.index') }}"
+                                                class="btn btn-secondary me-5 px-4 m-2">Cancel</a>
+                                            <!--end::Button-->
+                                            <!--begin::Button-->
+                                            <button type="submit" class="btn btn-primary" id="kt_client_submit">
+                                                <span class="indicator-label">Submit</span>
+                                            </button>
+                                            <!--end::Button-->
                                         </div>
                                     </div>
-                                </form>    
-                                </div>
-                                <!--end::Card body-->
+                                </form>
                             </div>
+                            <!--end::Card body-->
                         </div>
-                <!-- <div class="col-md-2"></div> -->
+                    </div>
+                    <!-- <div class="col-md-2"></div> -->
                 </div>
+            </div>
+            <!--end::Card body-->
         </div>
-        <!--end::Card body-->
-    </div>
-    <!--end::Card-->
+        <!--end::Card-->
     </div>
 </x-base-layout>
--- resources/views/master_data/user/index.blade.php
+++ resources/views/master_data/user/index.blade.php
@@ -1,94 +1,151 @@
 <x-base-layout>
     <style>
-        /* .btn:not(.btn-outline):not(.btn-dashed):not(.border-hover):not(.border-active):not(.btn-flush):not(.btn-icon){
-            margin-right: 15px !important;
-        } */
-        .dataTable tbody tr td {
-            padding: 0;
+        .btn-light-primary {
+            margin-left: 20px;
         }
 
-        .card .card-header {
-            min-height: 55px !important;
+        .btn:not(.btn-outline):not(.btn-dashed):not(.border-hover):not(.border-active):not(.btn-flush):not(.btn-icon) {
+            margin-right: 20px !important;
+        }
+
+        .dataTable tbody tr td {
+            padding: 0;
+            text-align: center !important;
+        }
+
+        .pb-3 {
+            padding-top: 0.75rem !important;
+        }
+
+        .min-w-175px {
+            min-width: 100px !important;
+        }
+
+        table.dataTable>thead>tr>th:not(.sorting_disabled) {
+            padding-right: 0;
+            padding-left: 0;
+            color: #1d0557;
+            /* Text color for th */
+            padding: 1rem 2rem;
+            /* Padding for th */
+        }
+
+        div.dataTables_wrapper div.dt-buttons {
+            float: right;
+        }
+
+        .btn.btn-secondary {
+            background-color: white !important;
+        }
+
+        .btn.btn-secondary:hover:not(.btn-active) {
+            color: #ffffff;
+            background-color: #1c0e40 !important;
+        }
+
+        ul.pagination {
+            float: right;
+        }
+
+        .excel-btn {
+            float: inline-end;
+            margin: -28px !important;
+        }
+
+        /* Add border to td */
+        table.dataTable th,
+        table.dataTable td {
+            border: 1px solid #EBEDF3;
+        }
+
+        .table> :not(:last-child)> :last-child>* {
+            border-bottom-color: #EBEDF3;
         }
     </style>
+
     <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
         <!--begin::Card header-->
         @if (Session::has('success'))
             <div class="alert alert-success">{{ Session::get('success') }}</div>
+            <script>
+                setTimeout(function () {
+                    document.querySelector('.alert').remove();
+                }, 3000); // Remove the alert after 3 seconds
+            </script>
         @endif
-        @if (Session::has('danger'))
-            <div class="alert alert-danger">{{ Session::get('danger') }}</div>
+        @if (Session::has('error'))
+            <div class="alert alert-danger">{{ Session::get('error') }}</div>
         @endif
         <div class="card-header cursor-pointer">
             <!--begin::Card title-->
-            <div class="card-title m-0 mb-3">
-                <div class="float-end mt-3 d-flex align-items-center">
-
-                    <h3 class="fw-bolder m-0 ms-5">{{ __('Users') }}</h3>
-                </div>
+            <div class="card-title m-0">
+                <h3 class="fw-bolder m-0">{{ __('Users') }}</h3>
             </div>
             <!--end::Card title-->
-
-            <!--begin::Action-->
             <a href="{{ theme()->getPageUrl('create_user') }}" class="btn btn-primary align-self-center"
                 style="padding: calc(0.775rem + 1px) calc(1.5rem + 1px) !important;">{{ __('Add User') }}</a>
-            <!--end::Action -->
+
+            <!--begin::Action-->
+
         </div>
+
         <!--begin::Card-->
-        <div class="card">
-            <!--begin::Card body-->
-            <div class="card-body pt-0">
-                <div class="table-responsive signing_fees">
-                    <table class="table dataTable " id="data-table">
-                        <!-- begin::Table head -->
-                        <thead>
+        <div class="card m-5 p-4">
+            <div class="card-body ">
 
-                            <tr class="fs-7 fw-bold text-gray-400 border-bottom-0">
-                                <th class="min-w-175px text-center">Id</th>
-                                <th class="min-w-175px text-center"> First&nbsp;Name</th>
-                                <th class="min-w-175px text-center">Last&nbsp;Name</th>
-                                <th class="min-w-175px text-center">Email</th>
-                                <th class="min-w-175px text-center">Role</th>
-                                <th class="min-w-175px text-center">Created&nbsp;at</th>
-                                <!-- <th class="min-w-175px text-center">Updated&nbsp;at</th> -->
-                                <th class="min-w-175px text-center">Action</th>
-                            </tr>
-                        </thead>
-                        <!-- end::Table head -->
-                        <!-- begin::Table body -->
-                        <tbody class="fixed">
-                            @foreach($users as $user)
-                                <tr>
-                                    <td class="p-0 pb-3 w-50px text-center">{{$user->id}}</td>
-                                    <td class="p-0 pb-3 w-50px text-center">{{$user->first_name }}</td>
-                                    <td class="p-0 pb-3 w-50px text-center">{{$user->last_name }}</td>
-                                    <td class="p-0 pb-3 w-50px text-center">{{$user->email}}</td>
-                                    <td class="p-0 pb-3 w-50px text-center">{{$user->role_id}}</td>
-                                    <td class="p-0 pb-3 w-50px text-center">{{$user->created_at}}</td>
-                                    <td class="p-0 pb-3 w-50px text-center">
-                                        <div class="d-flex justify-content-around">
-                                            <a href="{{ route('edit_user', $user->id) }}"
-                                                class="btn btn-light-primary font-weight-bold mr-2"> View </a>
-
-                                            <form action="{{ route('user.destroy', $user->id) }}" method="POST"
-                                                style="display: inline;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-light-danger font-weight-bold mr-2"
-                                                    onclick="return confirm('Are you sure to delete this record?')">Delete</button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                        <!-- end::Table body -->
-                    </table>
-                </div>
             </div>
-            <!--end::Card body-->
+            <div class="table-responsive signing_fees">
+                <table class="table dataTable " id="data-table">
+                    <!-- begin::Table head -->
+                    <thead>
+
+                        <tr class="fs-7 fw-bold text-gray-400 border-bottom-0">
+                            <th class="min-w-175px text-center">Id</th>
+                            <th class="min-w-175px text-center"> First&nbsp;Name</th>
+                            <th class="min-w-175px text-center">Last&nbsp;Name</th>
+                            <th class="min-w-175px text-center">Email</th>
+                            <th class="min-w-175px text-center">Role</th>
+                            <th class="min-w-175px text-center">Created&nbsp;at</th>
+                            <!-- <th class="min-w-175px text-center">Updated&nbsp;at</th> -->
+                            <th class="min-w-175px text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <!-- end::Table head -->
+                    <!-- begin::Table body -->
+                    <tbody class="fixed">
+                        @foreach($users as $user)
+                            <tr>
+                                <td class="p-0 pb-3 w-50px text-center">{{$user->id}}</td>
+                                <td class="p-0 pb-3 w-50px text-center">{{$user->first_name }}</td>
+                                <td class="p-0 pb-3 w-50px text-center">{{$user->last_name }}</td>
+                                <td class="p-0 pb-3 w-50px text-center">{{$user->email}}</td>
+                                <td class="p-0 pb-3 w-50px text-center">{{$user->role_id}}</td>
+                                <td class="p-0 pb-3 w-50px text-center">{{$user->created_at}}</td>
+                                <td class="p-0 pb-3 w-50px text-center">
+                                    <div class="d-flex justify-content-around">
+                                        <a href="{{ route('edit_user', $user->id) }}"
+                                            class="btn btn-light-primary font-weight-bold mr-2"> View </a>
+
+                                        <form action="{{ route('user.destroy', $user->id) }}" method="POST"
+                                            style="display: inline;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-light-danger font-weight-bold mr-2"
+                                                onclick="return confirm('Are you sure to delete this record?')">Delete</button>
+                                        </form>
+                                    </div>
+                                </td>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    <!-- end::Table body -->
+                </table>
+            </div>
         </div>
-        <!--end::Card-->
+        <!-- end::Card body -->
     </div>
+    <!-- end::Card -->
+    </div>
+
 </x-base-layout>
