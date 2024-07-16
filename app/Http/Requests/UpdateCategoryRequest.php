9a6ca810bcd13f86c40f556df8dafa5a4057e1b7
omkar-mahajan-dev <omkarmahajan0609@gmail.com>
2024-07-16 12:34:29 +0530

    category - update - upload icon functionality completed and updated record in upload_master table

--- app/Http/Requests/UpdateCategoryRequest.php
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -29,6 +29,12 @@ class UpdateCategoryRequest extends FormRequest
                 'string',
                 Rule::in(['0', '1']) // Ensure the status is either 0 or 1
             ],
+            'upload_icon' => [
+                'nullable', // Make the image upload optional
+                'image', // Ensure the file is an image
+                'mimes:jpeg,png,jpg,gif,svg', // Allow specific image types
+                'max:2048' // Max size in kilobytes (2MB)
+        ],
         ];
     }
 
@@ -36,6 +42,11 @@ class UpdateCategoryRequest extends FormRequest
     {
         return [
             'status.required' => 'The Status field is required.',
+            'status.string' => 'The Status must be a string.',
+            'status.in' => 'The selected Status is invalid.',
+            'upload_icon.image' => 'The Upload Icon must be an image.',
+            'upload_icon.mimes' => 'The Upload Icon must be a file of type: jpeg, png, jpg, gif, svg.',
+            'upload_icon.max' => 'The Upload Icon should not be greater than 2MB.',
         ];
     }
 }
